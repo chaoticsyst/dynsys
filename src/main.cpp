acf95d124566ee480ee0867671343504037a9a7f
Kirill Karnaukhov <bitikit@gmail.com>
2020-02-24 00:10:39 +0300

    add timer

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
     MainWindow w;
-    w.currentPoints = generate_points({1, 1, 1}, 10000, 10, 0.4); //It's random values for testing
+    w.currentPoints = generate_points({1, 1, 1}, 100000, 5, 0.45); //It's random values for testing
     w.show();
 
     return a.exec();
