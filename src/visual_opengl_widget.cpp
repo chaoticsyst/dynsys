eb40276c7b48b61474ff4a971eee0b54cf0e453b
Vladislav Nosivskoy <vladnosiv@gmail.com>
2020-02-25 13:59:30 +0300

    Add constants and comments for Kirill

--- src/visual_opengl_widget.cpp
+++ src/visual_opengl_widget.cpp
@@ -3,13 +3,32 @@
 #include "visual_opengl_widget.h"
 #include "Model.h"
 
+// Model constants
+const Point START_POINT = {1, 1, 1};
+const int COUNT_POINTS = 200'000;
+const int STEPS_PER_COUNT = 10;
+const double TAU = 0.001;
+
+// Timer constants
+const int TIMER_INTERVAL = 1;
+
+// Window size constants
+const int WINDOW_SIZE_H = 640;
+const int WINDOW_SIZE_W = 480;
+
+// Size of cubes (points)
+const double R = 0.005;
+
+// Normalize constans (TODO: adaptive normalize)
+const int DIV_NORMALIZE = 8;
+
 VisualOpenGLWidget::VisualOpenGLWidget(QWidget *parent) :
     QGLWidget{QGLFormat(), parent}, alpha{25}, beta{-25}, distance{5}, last{0} {
 
-    currentPoints = generate_points({1, 1, 1}, 200000, 10, 0.001);
+    currentPoints = generate_points(START_POINT, COUNT_POINTS, STEPS_PER_COUNT, TAU);
 
     timer = new QTimer(this);
-    timer->setInterval(1);
+    timer->setInterval(TIMER_INTERVAL);
     connect(timer, SIGNAL(timeout()), this, SLOT(updateTime()));
     timer->start();
 }
@@ -19,16 +38,15 @@ VisualOpenGLWidget::~VisualOpenGLWidget() {
 }
 
 QSize VisualOpenGLWidget::minimumSizeHint() const {
-    return QSize(640, 480);
+    return QSize(WINDOW_SIZE_H, WINDOW_SIZE_W);
 }
 
 QSize VisualOpenGLWidget::sizeHint() const {
-    return QSize(640, 480);
+    return QSize(WINDOW_SIZE_H, WINDOW_SIZE_W);
 }
 
 //TODO: rewrite this function
 void addCube(QVector<QVector3D> &vertices, const QVector3D &center) {
-    const double R = 0.005;
 
     //cubes
     vertices << QVector3D(center.x() - R, center.y() - R, center.z() + R)
@@ -84,7 +102,7 @@ void addCube(QVector<QVector3D> &vertices, const QVector3D &center) {
 }
 
 QVector3D pointFromStruct(const Point &point) {
-    return QVector3D(point.x / 8, point.y / 8, point.z / 8); //TODO: implement normalization
+    return QVector3D(point.x / DIV_NORMALIZE, point.y / DIV_NORMALIZE, point.z / DIV_NORMALIZE); //TODO: implement normalization
 }
 
 QVector<QVector3D> fromVector(const std::vector<Point> &points) {
@@ -99,7 +117,7 @@ void VisualOpenGLWidget::updateTime() {
     if (last == currentPoints.size()) {
         return;
     }
-    for (size_t i = 0; i < 10; i++) {
+    for (size_t i = 0; i < STEPS_PER_COUNT; i++) {
         addCube(vertices, pointFromStruct(currentPoints[last++]));
     }
     repaint();
@@ -132,7 +150,7 @@ void VisualOpenGLWidget::resizeGL(int width, int height) {
         height = 1;
     }
     pMatrix.setToIdentity();
-    pMatrix.perspective(60.0, (float) width / (float) height, 0.01, 1000);
+    pMatrix.perspective(60.0, (float) width / (float) height, 0.01, 1000); //Кирилл, разберись с константами, вообще непонятно
     glViewport(0, 0, width, height);
 }
 
@@ -143,18 +161,18 @@ void VisualOpenGLWidget::paintGL() {
     shaderProgram.bind();
 
     QMatrix4x4 cameraTransformation;
-    cameraTransformation.rotate(alpha, 0, 1, 0);
+    cameraTransformation.rotate(alpha, 0, 1, 0); //Тут желательно тоже разобраться с константами
     cameraTransformation.rotate(beta, 1, 0, 0);
 
     QVector3D cameraPosition = cameraTransformation * QVector3D(0, 0, distance);
-    QVector3D cameraUpDirection = cameraTransformation * QVector3D(0, 1, 0);
+    QVector3D cameraUpDirection = cameraTransformation * QVector3D(0, 1, 0); // И тут везде тоже
     vMatrix.lookAt(cameraPosition, QVector3D(0, 0, 0), cameraUpDirection);
 
     shaderProgram.setUniformValue("matrix", pMatrix * vMatrix * mMatrix);
     shaderProgram.setUniformValue("color", QColor(Qt::white));
     shaderProgram.setAttributeArray("vertex", vertices.constData());
     shaderProgram.enableAttributeArray("vertex");
-    glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+    glDrawArrays(GL_TRIANGLES, 0, vertices.size()); // У меня тут постоянно вылетает warning про устаршевий метод TODO: Заменить на более новый
     shaderProgram.disableAttributeArray("vertex");
     shaderProgram.release();
 }
@@ -171,7 +189,7 @@ void VisualOpenGLWidget::mouseMoveEvent(QMouseEvent *event) {
     if (event->buttons() & Qt::LeftButton) {
         alpha -= deltaX;
         while (alpha < 0) {
-            alpha += 360;
+            alpha += 360; // Это тоже вынести в константы
         }
         while (alpha >= 360) {
             alpha -= 360;
@@ -197,7 +215,7 @@ void VisualOpenGLWidget::wheelEvent(QWheelEvent *event) {
 
     if (event->orientation() == Qt::Vertical) {
         if (delta < 0) {
-            distance *= 1.1;
+            distance *= 1.1; // Эти константы тоже вынести
         } else if (delta > 0) {
             distance *= 0.9;
         }
