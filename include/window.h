d28ac3d190fc2597f218dd302c5cb20691bfe0c3
Vladislav Nosivskoy <vladnosiv@gmail.com>
2020-03-02 01:49:24 +0300

    Delete slots for one constant.

--- include/window.h
+++ include/window.h
@@ -16,10 +16,6 @@ public:
     ~Window();
 
 public slots:
-    void slot_set_constant_a(double){};//TODO: safe delete
-    void slot_set_constant_b(double){};//TODO: safe delete
-    void slot_set_constant_c(double){};//TODO: safe delete
-
     void slot_restart_button();
 
 protected:
