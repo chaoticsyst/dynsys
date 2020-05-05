6198087619ae27880cd29003ed2ddb9c0bcfaf56
Роман Венедиктов <e2e4b6b7@gmail.com>
2020-05-05 03:14:33 +0300

    Change tests architecture

--- test/testSystems.cpp
+++ test/testSystems.cpp
@@ -2,102 +2,56 @@
 #include "Model.hpp"
 #include "DynamicSystems/DynamicSystem.hpp"
 
-TEST(model, allConstantRK4_1) {
+int countConvergesSystems(const Model::Point &startPoint, int requiredCount, long double tau) {
     int convergesCount = 0;
 
+    int count = 0;
+    auto counter = [&count](const Model::Point &) {
+        ++count;
+    };
+
+    auto vectorSystems = DynamicSystems::getDefaultSystems<decltype(counter)>();
+
+    for (auto &system : vectorSystems) {
+        auto constantValues = system.getInterestingConstants();
+        for (auto&[name, params] : constantValues) {
+            count = 0;
+            system.compute(counter, startPoint, requiredCount, 1, tau, params);
+            if (count == requiredCount) convergesCount++;
+        }
+    }
+    return convergesCount;
+}
+
+TEST(model, allConstantRK4_1) {
     Model::Point startPoint = {0.1, 0.1, 0.1};
     int requiredCount = 100'000;
     long double tau = 0.01;
 
-    int count = 0;
-    auto counter = [&count](const Model::Point &) {
-        ++count;
-    };
-
-    auto vectorSystems = DynamicSystems::getDefaultSystems<decltype(counter)>();
-
-    for (auto &system : vectorSystems) {
-        auto constantValues = system.getInterestingConstants();
-        for (auto&[name, params] : constantValues) {
-            count = 0;
-            system.compute(counter, startPoint, requiredCount, 1, tau, params);
-            if (count == requiredCount) convergesCount++;
-        }
-    }
-    EXPECT_GE(convergesCount, 33);
+    EXPECT_GE(countConvergesSystems(startPoint, requiredCount, tau), 33);
 }
 
-TEST(model, allConstantRK4_2) {
-    int convergesCount = 0;
 
+TEST(model, allConstantRK4_2) {
     Model::Point startPoint = {0.01, 0.01, 0.01};
     int requiredCount = 500'000;
     long double tau = 0.01;
 
-    int count = 0;
-    auto counter = [&count](const Model::Point &) {
-        ++count;
-    };
-
-    auto vectorSystems = DynamicSystems::getDefaultSystems<decltype(counter)>();
-
-    for (auto &system : vectorSystems) {
-        auto constantValues = system.getInterestingConstants();
-        for (auto&[name, params] : constantValues) {
-            count = 0;
-            system.compute(counter, startPoint, requiredCount, 1, tau, params);
-            if (count == requiredCount) convergesCount++;
-        }
-    }
-    EXPECT_GE(convergesCount, 28);
+    EXPECT_GE(countConvergesSystems(startPoint, requiredCount, tau), 28);
 }
 
 TEST(model, allConstantRK4_3) {
-    int convergesCount = 0;
-
     Model::Point startPoint = {0.01, 0.01, 0.01};
     int requiredCount = 200'000;
     long double tau = 0.0001;
 
-    int count = 0;
-    auto counter = [&count](const Model::Point &) {
-        ++count;
-    };
-
-    auto vectorSystems = DynamicSystems::getDefaultSystems<decltype(counter)>();
-
-    for (auto &system : vectorSystems) {
-        auto constantValues = system.getInterestingConstants();
-        for (auto&[name, params] : constantValues) {
-            count = 0;
-            system.compute(counter, startPoint, requiredCount, 1, tau, params);
-            if (count == requiredCount) convergesCount++;
-        }
-    }
-    EXPECT_GE(convergesCount, 38);
+    EXPECT_GE(countConvergesSystems(startPoint, requiredCount, tau), 38);
 }
 
 TEST(model, allConstantRK4_4) {
-    int convergesCount = 0;
-
     Model::Point startPoint = {0.01, 0.01, 0.01};
     int requiredCount = 100'000;
     long double tau = 0.1;
 
-    int count = 0;
-    auto counter = [&count](const Model::Point &) {
-        ++count;
-    };
-
-    auto vectorSystems = DynamicSystems::getDefaultSystems<decltype(counter)>();
-
-    for (auto &system : vectorSystems) {
-        auto constantValues = system.getInterestingConstants();
-        for (auto&[name, params] : constantValues) {
-            count = 0;
-            system.compute(counter, startPoint, requiredCount, 1, tau, params);
-            if (count == requiredCount) convergesCount++;
-        }
-    }
-    EXPECT_GE(convergesCount, 20);
+    EXPECT_GE(countConvergesSystems(startPoint, requiredCount, tau), 20);
 }
